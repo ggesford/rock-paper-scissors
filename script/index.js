b0ce54deda4ab51202f854b2c2ae975f35a278b9
ggesford <gageg0411@gmail.com>
2024-11-30 22:29:19 -0800

    half write getComputerChoice function

---
+++ script/index.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    return Math.floor(Math.random()*3)+1;
+} 
+//^^random 1-3 number generator
+ console.log(getComputerChoice());
+
+getHumanChoice = prompt("Rock, Paper, or Scissors?", "");
+
+
+
