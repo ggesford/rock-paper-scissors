c7808ebea5fda57a872558a63c167be76fb3cfaf
ggesford <gageg0411@gmail.com>
2025-01-08 15:54:04 -0800

    implement button use for player input;refactor round;comment div;

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
-let humanScore = 0
-
-let computerScore = 0
+let humanScore = 0;
+let computerScore = 0;
+let scores = `SCORES: YOU: ${humanScore}, CPU: ${computerScore}`
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random()*3)+1;
@@ -9,50 +9,48 @@ function getComputerChoice() {
     } else if (choice == 2) {
         return "paper";
     } else {
-        return "scissors"
-    }
-} 
-//^^random 1-3 number generator
-
-function getHumanChoice() {
-   let getHumanChoice = prompt("rock, paper, or scissors?", "").toLowerCase();
-   return getHumanChoice;
-}
+        return "scissors";
+    };
+} ;
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice == humanChoice ) {
-        console.log("It's a tie");
+        div.textContent = "Woah, it's a tie!"
     } else if (computerChoice == "rock" && humanChoice == "paper") {
-        console.log("you win duder, your paper suffocated the enemies rock");
+        div.textContent = "you win duder, your paper suffocated the enemies rock";
         ++humanScore;
-    } else if (computerChoice == "rock" && humanChoice == "scissors") {
-        console.log("your scissors were crushed by their rock, you lose");
-        ++computerScore;
-    } else if (computerChoice == "scissors" && humanChoice == "rock") {
-        console.log("your rock destroyed their scissors");
-        ++humanScore;
-    } else if (computerChoice == "scissors" && humanChoice == "paper") {
-        console.log("Their scissors sliced your paper up into tiny pieces");
-        ++computerScore;
     } else if (computerChoice == "paper" && humanChoice == "scissors") {
-        console.log("your valiant scissors defended your honor against the enemies most foul paper");
+        div.textContent = "your valiant scissors defended your honor against the enemies most foul paper";
         ++humanScore;
-    } else if (computerChoice == "paper" && humanChoice == "rock") {
-        console.log("the enemies paper strangled your rock to death... wtf");
+    } else if (computerChoice == "scissors" && humanChoice == "rock") {
+        div.textContent = "your rock crushes their scissors' skull to dust";
+        ++humanScore;
+    } else {
         ++computerScore;
-    }
-       
-}
+        div.textContent = "They won... wtf tho...";
+    };
+};
 
 function playGame() {
-    let humanChoice = getHumanChoice();
-    let computerchoice = getComputerChoice();
-    console.log(`you chose ${humanChoice} and the cpu chose ${computerchoice}`);
-    playRound(computerchoice, humanChoice);
-    console.log(`SCORES: YOU: ${humanScore}, CPU: ${computerScore}`)
+    let computerChoice = getComputerChoice();
+    console.log(`you chose ${humanChoice} and the cpu chose ${computerChoice}`);
+    playRound(computerChoice, humanChoice);
+    console.log(`SCORES: YOU: ${humanScore}, CPU: ${computerScore}`);
     
-}
+};
 
+const choiceButtons = document.querySelectorAll(".choiceButton");
+const div = document.querySelector("div");
+
+choiceButtons.forEach((choiceButtons) => {
+    choiceButtons.addEventListener("click", (event) => {
+        playRound(getComputerChoice(), event.target.id); 
+
+    });
+});
+
+/*
 for (let i = 0; i <5; ++i) {
     playGame();
-}
+};
+*/
